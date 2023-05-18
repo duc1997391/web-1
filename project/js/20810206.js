526e719ae1fed0f7816c0f7c36343412856786f9
duc.tran <duc199739100@gmail.com>
2023-05-18 23:18:43 +0700

    update

---
+++ project/js/20810206.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+  activeMenu();
+});
+
+function activeMenu() {
+  const href = window.location.pathname.split("/").pop();
+  const menuIds = ["app-menu", "menu-footer"];
+
+  menuIds.forEach((id) => {
+    const headerMenu = document.getElementById(id);
+    const headerMenuItems = headerMenu.getElementsByClassName("nav-link");
+    
+    for (let index = 0; index < headerMenuItems.length; index++) {
+      const element = headerMenuItems[index];
+      if (element.attributes.href.value === href) {
+        element.classList.add("active");
+      }
+    }
+  });
+}
