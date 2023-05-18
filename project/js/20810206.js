38a34c5ed866b482a1f0e457361286899711c143
duc.tran <duc199739100@gmail.com>
2023-05-18 23:28:38 +0700

    update

--- project/js/20810206.js
+++ project/js/20810206.js
@@ -4,12 +4,13 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 function activeMenu() {
   const href = window.location.pathname.split("/").pop();
+  console.log(href)
   const menuIds = ["app-menu", "menu-footer"];
 
   menuIds.forEach((id) => {
     const headerMenu = document.getElementById(id);
     const headerMenuItems = headerMenu.getElementsByClassName("nav-link");
-    
+
     for (let index = 0; index < headerMenuItems.length; index++) {
       const element = headerMenuItems[index];
       if (element.attributes.href.value === href) {
