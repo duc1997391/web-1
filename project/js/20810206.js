bf68581bca96d115d927000bb60e6402f1cb62a4
duc.tran <duc199739100@gmail.com>
2023-05-21 17:06:25 +0700

    contact

--- project/js/20810206.js
+++ project/js/20810206.js
@@ -1,6 +1,12 @@
 document.addEventListener("DOMContentLoaded", function (event) {
   activeMenu();
 });
+const API = `https://web1-api.vercel.app/api/`
+const USER_API = `https://web1-api.vercel.app/users`
+const HeaderJson = {
+  'Content-Type': 'application/json',
+  'Accept': 'application/json',
+}
 
 function activeMenu() {
   const href = window.location.pathname.split("/").pop() || 'index.html';
@@ -20,7 +26,6 @@ function activeMenu() {
 }
 
 async function loadData (path, templateId, containerId) {
-  const API = `https://web1-api.vercel.app/api/`
   const response = await fetch(API + path)
   const data = await response.json()
 
@@ -28,4 +33,17 @@ async function loadData (path, templateId, containerId) {
   const template = Handlebars.compile(source)
   const html = template({ data })
   document.getElementById(containerId).innerHTML = html
+}
+
+async function getAuthToken(username, password) {
+  const response = await fetch(`${USER_API}/authenticate`, {
+    method: 'POST',
+    headers: HeaderJson,
+    body: JSON.stringify({username, password})
+  })
+  const result = await response.json()
+  if (response.status === 200) {
+    return result.token;
+  }
+  throw new Error(response.message)
 }
